44faa48f761db4ee77c56192bf972f9d15b519e0
Hiroshi Kimura <hkim@hkim.jp>
2025-05-04 16:08:56 +0900

    under construction

--- src/konpy/answers.clj
+++ src/konpy/answers.clj
@@ -1,9 +1,24 @@
 (ns konpy.answers
   (:require
+   [hiccup2.core :as h]
+   [ring.util.anti-forgery :refer [anti-forgery-field]]
+   [konpy.utils :refer [user]]
    [konpy.views :refer [page under-construction-page]]))
 
-(defn answer [_request]
-  (under-construction-page nil))
+(defn answer [request]
+  (page
+    [:div
+     [:div "name: " (user request)]
+     [:div "Q:" ]
+     [:div
+      [:form {:method "post"}
+       (h/raw (anti-forgery-field))
+       [:input {:type "hidden" :name "login" :value (user request)}]
+       [:div [:textarea {:class "s-300"
+                         :name "answer"} "your answer"]]
+       [:div [:button {:class "bg-red"} "submit"]]]]]))
 
-(defn answer! [_request]
-  (under-construction-page nil))
+(defn answer! [{params :params}]
+  (let [params (dissoc params :__anti-forgery-token)]
+    (page
+      [:div (str params)])))
