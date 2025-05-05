394c52eb330aecb9f9dc402d9653e30b141185f3
hkimjp <hkim@hkim.jp>
2025-05-05 22:00:35 +0900

    0.6.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,7 +4,7 @@
 - (set! *default-data-reader-fn* tagged-literal)
 - konpy.db wrapper?
 
-## 0.6.1-SNAPSHOT
+## 0.6.1 (2025-05-05)
 
 - utils/sha1
 - utils/remove-spaces
--- src/konpy/views.clj
+++ src/konpy/views.clj
@@ -2,7 +2,7 @@
   (:require [hiccup2.core :as h]
             [ring.util.response :as response]))
 
-(def ^:private version "0.6.0")
+(def ^:private version "0.6.1")
 
 (defn base
   [content]
