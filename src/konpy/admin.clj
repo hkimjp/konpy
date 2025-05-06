8af81b7676e060ba6006002fbcbe527406020bda
hkimjp <hkim@hkim.jp>
2025-05-06 09:54:43 +0900

    can create tasks.

--- src/konpy/admin.clj
+++ src/konpy/admin.clj
@@ -1,12 +1,12 @@
 (ns konpy.admin
   (:require [taoensso.telemere :as t]
-   [hiccup2.core :as h]
-   [ring.util.response :as resp]
-   [ring.util.anti-forgery :refer [anti-forgery-field]]
+            [hiccup2.core :as h]
+            [ring.util.response :as resp]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]
    ; [environ.core :refer [env]]
-   [konpy.db :refer [put! q]]
-   [konpy.utils :refer [now]]
-   [konpy.views :refer [page  under-construction-page]]))
+            [konpy.db :refer [put! q]]
+            [konpy.utils :refer [now]]
+            [konpy.views :refer [page  under-construction-page]]))
 
 (defn tasks
   "list all the tasks. edit, delete and add."
@@ -19,44 +19,51 @@
                   [?e :task ?task]
                   [?e :issued ?issued]]
         ret (->> (q tasks-q)
-              (sort-by (juxt :week :num)))]
+                 (sort-by (juxt :week :num)))]
     (page
-      [:div
-       [:div [:a {:href "/tasks"} "->tasks"]]
-       (for [{:keys [e week num task]}
-             (conj ret {:e -1 :week "" :num "" :task ""})]
-         [:div {:class "flex"}
-          [:form {:class "mx-xl"}
-           [:div {:class "flex items-center"}
-            [:input {:type "hidden" :name "e" :value e}]
-            [:input {:class "text-center size-10 shadow-lg outline outline-black/5"
-                     :value week}]
-            " - "
-            [:input {:class "text-center size-10 shadow-lg outline outline-black/5"
-                     :value num}]
-            [:textarea {:class "w-120 outline outline-black/5 shadow-lg"}
-             task]
-            [:button {:class "rounded-full bg-sky-200 hover:bg-sky-500 active:bg-red-500"}
-             "update"]]]])
-       [:div {:class "flex"}
-        [:p [:a {:href "/admin/new"
-                 :class "p-1 rounded-sm bg-sky-200 hover:bg-sky-500 active:bg-red-500"}
-             "new"]]]])))
+     [:div
+      [:div [:a {:href "/tasks"
+                 :class "rounded-xl text-white bg-red-500 hover:bg-red-700 active:bg-red-500"}
+             "tasks"]]
+      (for [{:keys [e week num task]}
+            (conj ret {:e -1 :week "" :num "" :task ""})]
+        [:div {:class "flex"}
+         [:form {:class "mx-xl" :method "post"}
+          (h/raw (anti-forgery-field))
+          [:div {:class "flex items-center"}
+           [:input {:type "hidden" :name "e" :value e}]
+           [:input {:class "text-center size-10 shadow-lg outline outline-black/5"
+                    :name "week"
+                    :value week}]
+           " - "
+           [:input {:class "text-center size-10 shadow-lg outline outline-black/5"
+                    :name "num"
+                    :value num}]
+           [:textarea {:class "w-120 outline outline-black/5 shadow-lg"
+                       :name "task"}
+            task]
+           [:button {:class "rounded-xl text-white bg-sky-500 hover:bg-sky-700 active:bg-red-500"}
+            "update"]]]])
+      [:div {:class "flex"}
+       [:p [:a {:href "/admin/new"
+                :class "rounded-xl text-white bg-sky-500 hover:bg-sky-700 active:bg-red-500"}
+            "new"]]]])))
 
+; use?
 (defn new [_]
   (page
-    [:div
-     [:div {:class "text-4xl"} "new task"]
-     [:form {:method "post"}
-      (h/raw (anti-forgery-field))
-      [:textarea {:class ""
-                  :placeholder "new task"
-                  :name "task"}]
-      [:input {:name "week" :value 1}]
-      [:input {:name "num" :value 1}]
-      [:button "create"]]]))
+   [:div
+    [:div {:class "text-4xl"} "new task"]
+    [:form {:method "post"}
+     (h/raw (anti-forgery-field))
+     [:textarea {:class ""
+                 :placeholder "new task"
+                 :name "task"}]
+     [:input {:name "week" :value 1}]
+     [:input {:name "num" :value 1}]
+     [:button "create"]]]))
 
-(defn put-task! [^long week ^long num ^String task]
+(defn create-task! [^long week ^long num ^String task]
   (put! [{:db/add -1
           :week week
           :num  num
@@ -64,8 +71,8 @@
           :issued (now)}]))
 
 (defn create! [{{:keys [week num task]} :params}]
-  (t/log! {:level :info :data {:task task}} "create!")
-  (put-task! (parse-long week) (parse-long num) task)
+  (t/log! {:level :info :data {:week week :num num :task task}} "create!")
+  (create-task! (parse-long week) (parse-long num) task)
   (resp/redirect "/admin"))
 
 (defn edit [_]
@@ -74,5 +81,6 @@
 (defn edit! [_]
   (under-construction-page nil))
 
+;; no delete?
 (defn delete! [_]
   (under-construction-page nil))
