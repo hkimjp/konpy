ffca74fb85485f669b62b89b923284ac9736e2c2
Hiroshi Kimura <hkim@hkim.jp>
2025-05-08 11:15:35 +0900

    removed some parts from pom.xml

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'io.github.hkimjp/konpy)
-(def version "0.7.4")
+(def version "0.7.5-SNAPSHOT")
 (def main 'konpy.core)
 (def class-dir "target/classes")
 
