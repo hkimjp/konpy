a8a298f679add0ee468bbbabf9851e153b9e2d20
Hiroshi Kimura <hkim@hkim.jp>
2025-05-08 15:57:31 +0900

    recent logins from `konpy.log`

--- src/konpy/views.clj
+++ src/konpy/views.clj
@@ -2,7 +2,7 @@
   (:require [hiccup2.core :as h]
             [ring.util.response :as response]))
 
-(def ^:private version "0.7.6-SNAPSHOT")
+(def ^:private version "0.7.6")
 
 (defn base
   [content]
