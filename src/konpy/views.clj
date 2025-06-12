8909eeb4bc9bbdb2be3c919437a14981fa5cc59f
Hiroshi Kimura <hkim@hkim.jp>
2025-06-12 11:54:42 +0900

    fixed the blacklist feature

--- src/konpy/views.clj
+++ src/konpy/views.clj
@@ -2,7 +2,7 @@
   (:require [hiccup2.core :as h]
             [ring.util.response :as response]))
 
-(def ^:private version "0.17.0-SNAPSHOT")
+(def ^:private version "0.17.1")
 
 (defn base
   [content]
