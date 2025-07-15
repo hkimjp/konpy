b3fe0ef5aa72855d6216765b817afc2b8b975781
Hiroshi Kimura <hkim@hkim.jp>
2025-07-15 16:26:30 +0900

    goods_bads.clj

--- src/konpy/goods_bads.clj
+++ src/konpy/goods_bads.clj
@@ -11,12 +11,12 @@
 
 (defn who-sent-how-many [what]
   (->> (who-sent what)
-    (group-by identity)
-    (map (fn [[k v]] [k (count v)]))))
+       (group-by identity)
+       (map (fn [[k v]] [k (count v)]))))
 
 (comment
   (pprint
-    (reverse (sort-by (fn [x] (second x)) (who-sent-how-many "good"))))
+   (reverse (sort-by (fn [x] (second x)) (who-sent-how-many "good"))))
   (pprint
-    (reverse (sort-by (fn [x] (second x)) (who-sent-how-many "bad"))))
+   (reverse (sort-by (fn [x] (second x)) (who-sent-how-many "bad"))))
   :rcf)
