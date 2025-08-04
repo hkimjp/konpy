e6803a9491a8fa2bdf8919a21e3fe404d2474cb4
Hiroshi Kimura <hkim@hkim.jp>
2025-08-04 09:14:05 +0900

    filename to download

--- build.clj
+++ build.clj
@@ -3,7 +3,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'io.github.hkimjp/konpy)
-(def version "0.30.0")
+(def version "0.31.0")
 (def main 'konpy.core)
 (def class-dir "target/classes")
 
